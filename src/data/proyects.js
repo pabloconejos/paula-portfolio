6e1c29ddd2402bdc81500038f856c80362892475
Pablo Conejos <pconejos03@gmail.com>
2025-10-19 17:52:14 +0200

    commit

--- src/data/proyects.js
+++ src/data/proyects.js
@@ -3,14 +3,24 @@ export const projects = [
   {
     id: "iberia",
     title: "IBERIA",
-    images: ["/fotos/proyectos/iberia.jpg"],
+    images: [
+      '/fotos/proyectos/IBERIA/2.png',
+      '/fotos/proyectos/IBERIA/3.png',
+      '/fotos/proyectos/IBERIA/4.png',
+      '/fotos/proyectos/IBERIA/12.png',
+      '/fotos/proyectos/IBERIA/15.png',
+      '/fotos/proyectos/IBERIA/24.png',
+    ],
     subTitulos: ["Creatividad en Vivo", "Equipo BRAVA", "2025"],
-    video_concept: "video/videoporfolio.MOV",
+    video_concept: [
+      "/fotos/proyectos/IBERIA/IBERIA.MOV",
+      "/fotos/proyectos/IBERIA/iberia x brava agency.mp4",
+    ],
     description: [
       `Durante el Seminario de Creatividad en Vivo organizado por la Universitat Jaume I junto a McCann España e Iberia, Brava Agency desarrolló el proyecto “Ciudadanos del Mundo”, una campaña que redefine la forma de viajar.
-Partiendo de la idea de que hoy se viaja más pero con menos conexión real, la propuesta invita a viajar menos, pero mejor, fomentando experiencias conscientes, culturales y transformadoras.
-La iniciativa se concreta en el Espacio Iberia Tour, un lugar para redescubrir el mundo mediante el arte, los oficios y las comunidades locales.
-Con ello, Iberia evoluciona de ser una aerolínea a convertirse en una marca que inspira una nueva manera de viajar.`,
+      Partiendo de la idea de que hoy se viaja más pero con menos conexión real, la propuesta invita a viajar menos, pero mejor, fomentando experiencias conscientes, culturales y transformadoras.
+      La iniciativa se concreta en el Espacio Iberia Tour, un lugar para redescubrir el mundo mediante el arte, los oficios y las comunidades locales.
+      Con ello, Iberia evoluciona de ser una aerolínea a convertirse en una marca que inspira una nueva manera de viajar.`,
     ],
     link: "/project",
     video_concept_credits: "Videoconcept realizado por Paula Alberola Zaragozá",
@@ -19,8 +29,19 @@ Con ello, Iberia evoluciona de ser una aerolínea a convertirse en una marca que
     id: "moeve",
     title: "MOEVE",
     subTitulos: ["Premios Eficacia Universidades", "Equipo BRAVA", "2025"],
-    images: ["/fotos/proyectos/genMoeve.jpg"],
-    video_concept: "video/videoporfolio.MOV",
+    images: [
+      '/fotos/proyectos/moeve/2.png',
+      '/fotos/proyectos/moeve/3.png',
+      '/fotos/proyectos/moeve/4.png',
+      '/fotos/proyectos/moeve/5.png',
+      '/fotos/proyectos/moeve/6.png',
+      '/fotos/proyectos/moeve/7.png',
+      '/fotos/proyectos/moeve/9.png',
+      '/fotos/proyectos/moeve/19.png',
+      '/fotos/proyectos/moeve/20.png',
+      '/fotos/proyectos/moeve/22.png',
+    ],
+    video_concept: ["VIDEOCONCEPT MOEVE 2.MP4"],
     description: [
       `Brava Agency ganó los Premios Eficacia Universidades 2025 con Gen Moeve, una propuesta de branded content creada para Moeve, la marca joven de Cepsa enfocada en energías sostenibles.
 La campaña, concebida por y para jóvenes, responde al estigma de la “Generación de Cristal” reivindicando su fortaleza y compromiso social.
@@ -33,8 +54,19 @@ Gen Moeve se consolidó como un movimiento de pensamiento crítico y empoderamie
   {
     id: "papajhons",
     title: "PAPA JHONS",
-    video_concept: "video/videoporfolio.MOV",
-    images: ["/fotos/proyectos/papajhons.jpg"],
+    video_concept: [
+      "/fotos/proyectos/PAPA JOHNS/papaJhons_video_concept.MOV",
+    ],
+    images: [
+      '/fotos/proyectos/PAPA JOHNS/1.png',
+      '/fotos/proyectos/PAPA JOHNS/23.png',
+      '/fotos/proyectos/PAPA JOHNS/24.png',
+      '/fotos/proyectos/PAPA JOHNS/26.png',
+      '/fotos/proyectos/PAPA JOHNS/28.png',
+      '/fotos/proyectos/PAPA JOHNS/31.png',
+      '/fotos/proyectos/PAPA JOHNS/34.png',
+
+    ],
     subTitulos: ["Universidad Jaume I", "Equipo BRAVA", "2024"],
     description: [
       `Para celebrar la apertura de la tienda número 100 de Papa John’s en España, Brava Agency creó la campaña “Dame una pizta, papá”, dirigida al público joven y basada en el tono cercano y humorístico de la marca.
@@ -45,4 +77,45 @@ Desarrollada como un reto universitario, la campaña fue ganadora del concurso p
     link: "/project",
     video_concept_credits: "Videoconcept realizado por Paula Alberola Zaragozá",
   },
+  {
+    id: "milfshakes",
+    title: "MILFSHAKES",
+    video_concept: [
+      "/fotos/proyectos/milfshakes/videocase milfshakes.mp4",
+      "/fotos/proyectos/milfshakes/Video dupla.mp4",
+    ],
+    images: [
+      '/fotos/proyectos/milfshakes/2.png',
+    ],
+    subTitulos: [],
+    description: [],
+    link: "/milfshakes",
+    video_concept_credits: "",
+  },
+  {
+    id: "setmestre",
+    title: "SET MESTRE",
+    video_concept: [],
+    images: [
+      '/fotos/proyectos/moodboard teka/5.png',
+      '/fotos/proyectos/moodboard teka/6.png',
+      '/fotos/proyectos/moodboard teka/7.png',
+      '/fotos/proyectos/moodboard teka/9.png',
+      '/fotos/proyectos/moodboard teka/hecho.png',
+    ],
+    subTitulos: [],
+    description: [],
+    link: "/setmestre",
+    video_concept_credits: "",
+  }
+  // {
+  //   id: "",
+  //   title: "",
+  //   video_concept: [],
+  //   images: [],
+  //   subTitulos: [],
+  //   description: [],
+  //   link: "",
+  //   video_concept_credits: "",
+  // }
 ];
