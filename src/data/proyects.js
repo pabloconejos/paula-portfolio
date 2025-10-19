c608db06838d6525e0746c641fbcef367bdf2d19
Pablo Conejos <pconejos03@gmail.com>
2025-10-19 20:18:44 +0200

    commit

--- src/data/proyects.js
+++ src/data/proyects.js
@@ -3,34 +3,38 @@ export const projects = [
   {
     id: "iberia",
     title: "IBERIA",
+    titulo_detalle: "Para los viajeros del mundo",
     images: [
-      '/fotos/proyectos/IBERIA/2.png',
       '/fotos/proyectos/IBERIA/3.png',
       '/fotos/proyectos/IBERIA/4.png',
       '/fotos/proyectos/IBERIA/12.png',
-      '/fotos/proyectos/IBERIA/15.png',
-      '/fotos/proyectos/IBERIA/24.png',
     ],
-    subTitulos: ["Creatividad en Vivo", "Equipo BRAVA", "2025"],
+    subTitulos: ["1º Premio Creatividad en Vivo", "Universidad Jaume I x McCann España", "2025"],
     video_concept: [
       "/fotos/proyectos/IBERIA/IBERIA.MOV",
-      "/fotos/proyectos/IBERIA/iberia x brava agency.mp4",
+      "/fotos/proyectos/IBERIA/Iberia x brava agency.mp4",
     ],
+    poster: "/fotos/proyectos/IBERIA/iberia_portada.png",
+    poster2: "/fotos/proyectos/IBERIA/portada2.jpg",
     description: [
-      `Durante el Seminario de Creatividad en Vivo organizado por la Universitat Jaume I junto a McCann España e Iberia, Brava Agency desarrolló el proyecto “Ciudadanos del Mundo”, una campaña que redefine la forma de viajar.
-      Partiendo de la idea de que hoy se viaja más pero con menos conexión real, la propuesta invita a viajar menos, pero mejor, fomentando experiencias conscientes, culturales y transformadoras.
-      La iniciativa se concreta en el Espacio Iberia Tour, un lugar para redescubrir el mundo mediante el arte, los oficios y las comunidades locales.
-      Con ello, Iberia evoluciona de ser una aerolínea a convertirse en una marca que inspira una nueva manera de viajar.`,
+      `Para la edición de <b>Creatividad en Vivo 2025</b>, se nos propuso crear un nuevo espacio Iberia. Pero en un momento en el que volar es más fácil que nunca, nos preguntamos algo esencial: ¿realmente viajamos mejor que antes? Hoy cogemos más vuelos, cruzamos más fronteras, pero conectamos menos con los lugares a los que llegamos.`,
+      `Por eso decidimos romper con lo establecido y proponer una campaña provocadora: <b>este año no habría un espacio Iberia, sino muchos</b>. Apadrinamos pequeños negocios y locales ligados al viaje con el objetivo de que Iberia se convirtiera en la marca que invita a <b>viajar con calma, con propósito, con conexión</b>. Porque para nosotros no se trataba de viajar más, sino de <b>viajar mejor</b>. Una campaña que reescribe la manera de volar y nos recuerda que, a veces, moverse menos hace que podamos llegar más lejos.`,
     ],
     link: "/project",
     video_concept_credits: "Videoconcept realizado por Paula Alberola Zaragozá",
+    creditos: [
+      "Un proyecto desarrollado por <b>BRAVA</b>:",
+      "<b>Dirección de Arte:</b> Paula Alberola y Paula Abella Tello",
+      "<b>Investigación:</b> Ignacio Cervera Rodrigo y Marta Bueno Montalt",
+      "<b>Planning:</b> Beatriz Baena Martínez",
+    ]
   },
   {
     id: "moeve",
     title: "MOEVE",
-    subTitulos: ["Premios Eficacia Universidades", "Equipo BRAVA", "2025"],
+    titulo_detalle: "Gen Moeve X Moeve",
+    subTitulos: ["1º Premio Eficacia Universidades", "Premios Eficacia", "2025"],
     images: [
-      '/fotos/proyectos/moeve/2.png',
       '/fotos/proyectos/moeve/3.png',
       '/fotos/proyectos/moeve/4.png',
       '/fotos/proyectos/moeve/5.png',
@@ -41,24 +45,31 @@ export const projects = [
       '/fotos/proyectos/moeve/20.png',
       '/fotos/proyectos/moeve/22.png',
     ],
-    video_concept: ["VIDEOCONCEPT MOEVE 2.MP4"],
+    video_concept: ["/fotos/proyectos/moeve/VIDEOCONCEPT MOEVE 2.MP4"],
+    poster: "/fotos/proyectos/moeve/moeve_portada.png",
     description: [
-      `Brava Agency ganó los Premios Eficacia Universidades 2025 con Gen Moeve, una propuesta de branded content creada para Moeve, la marca joven de Cepsa enfocada en energías sostenibles.
-La campaña, concebida por y para jóvenes, responde al estigma de la “Generación de Cristal” reivindicando su fortaleza y compromiso social.
-A través de un reto real —25 estudiantes, 5 equipos, 5 ciudades y un objetivo común—, se impulsó la creación de soluciones a problemas sociales desde la acción colectiva.
-Gen Moeve se consolidó como un movimiento de pensamiento crítico y empoderamiento juvenil, donde la marca acompaña sin protagonizar, reflejando una comunicación auténtica y participativa.`,
+      `GenMoeve fue la <b>estrategia ganadora de los Premios Eficacia Universidades 2025 para Moeve</b>. Como valencianos, tras vivir la DANA, pensamos: qué pena que haga falta una desgracia para que se den cuenta de lo valientes que somos. De esa reflexión nació una estrategia que pone a los jóvenes al mando del cambio a partir del nacimiento de oportunidades, y no de desgracias.`,
+      `Creamos <b>GenMoeve</b>, un programa donde <b>25 jóvenes</b> se enfrentan al desafío de crear soluciones innovadoras para problemáticas reales, donde el proyecto ganador se llevaría a cabo <b>de la mano de Moeve</b>. Una iniciativa que convierte a la marca en impulsora de talento, y a los jóvenes, en los verdaderos <b>precursores del cambio</b>.`,
+
     ],
     link: "/project",
     video_concept_credits: "Videoconcept realizado por Paula Alberola Zaragozá",
+    creditos: [
+      "Un proyecto creado por <b>BRAVA</b>:",
+      "<b>Dirección de Arte:</b> Paula Alberola y Paula Abella Tello",
+      "<b>Investigación:</b> Ignacio Cervera Rodrigo y Marta Bueno Montalt",
+      "<b>Planning:</b> Beatriz Baena Martínez",
+    ]
   },
   {
     id: "papajhons",
-    title: "PAPA JHONS",
+    title: "PAPA JOHN'S",
+    titulo_detalle: "Dame una pizta, papá",
     video_concept: [
       "/fotos/proyectos/PAPA JOHNS/papaJhons_video_concept.MOV",
     ],
+    poster: "/fotos/proyectos/PAPA JOHNS/1.png",
     images: [
-      '/fotos/proyectos/PAPA JOHNS/1.png',
       '/fotos/proyectos/PAPA JOHNS/23.png',
       '/fotos/proyectos/PAPA JOHNS/24.png',
       '/fotos/proyectos/PAPA JOHNS/26.png',
@@ -67,7 +78,7 @@ Gen Moeve se consolidó como un movimiento de pensamiento crítico y empoderamie
       '/fotos/proyectos/PAPA JOHNS/34.png',
 
     ],
-    subTitulos: ["Universidad Jaume I", "Equipo BRAVA", "2024"],
+    subTitulos: ["1º Premio Concurso de Talentos", "Papa John's x Rosebud", "Equipo BRAVA", "2024"],
     description: [
       `Para celebrar la apertura de la tienda número 100 de Papa John’s en España, Brava Agency creó la campaña “Dame una pizta, papá”, dirigida al público joven y basada en el tono cercano y humorístico de la marca.
 La acción, de enfoque transmedia y gamificado, comenzó con un teaser viral en TikTok y continuó con una plataforma interactiva donde los usuarios podían votar la ubicación de la nueva tienda obteniendo pistas mediante sus compras.
@@ -76,37 +87,68 @@ Desarrollada como un reto universitario, la campaña fue ganadora del concurso p
     ],
     link: "/project",
     video_concept_credits: "Videoconcept realizado por Paula Alberola Zaragozá",
+    creditos: [
+      "Un proyecto desarrollado por <b>BRAVA</b>:",
+      "<b>Dirección de Arte:</b> Paula Alberola y Paula Abella Tello",
+      "<b>Investigación:</b> Ignacio Cervera Rodrigo y Marta Bueno Montalt",
+      "<b>Planning:</b> Beatriz Baena Martínez",
+    ]
   },
   {
     id: "milfshakes",
     title: "MILFSHAKES",
+    titulo_detalle: "Los Inapropiados",
     video_concept: [
       "/fotos/proyectos/milfshakes/videocase milfshakes.mp4",
       "/fotos/proyectos/milfshakes/Video dupla.mp4",
     ],
     images: [
-      '/fotos/proyectos/milfshakes/2.png',
     ],
-    subTitulos: [],
-    description: [],
+    poster: "/fotos/proyectos/milfshakes/milfshakes_portada.png",
+    poster2: "/fotos/proyectos/moodboard teka/9.png",
+    subTitulos: ["Los Inapropiados", "Concurso Jóvenes Talentos IAB Spain x Milfshakes", "Dupla LADOBLEP", "2025"],
+    description: [
+      `Presentada en el certamen de <b>Jóvenes Talentos de IAB Spain 2025, “Los Inapropiados”</b> fue nuestra propuesta para una colaboración entre <b>Milfshakes y Chupa Chups</b>: En un mundo que exige perfección, nosotros quisimos celebrar a quienes se equivocan, meten la pata o sueltan lo que no toca… porque, al final, son ellos los que convierten lo incómodo en inolvidable. Así que en lugar de corregirlos, <b>decidimos contratarlos</b>.`,
+      `La acción arrancaba con un falso combate en el centro de Madrid. Desde ahí, la campaña se desplegaba con un drop misterioso, máquinas expendedoras escondidas por la ciudad y una dinámica digital que invitaba a demostrar lo inapropiado que uno podía llegar a ser.`,
+      `Y como todo trabajo merece su recompensa, el ganador se llevaba <b>un sueldo de por vida</b>, porque si ser inapropiado es el trabajo más complicado y, al mismo tiempo, el más memorable, <b>qué menos que hacerlo rentable</b>.`
+    ],
     link: "/milfshakes",
     video_concept_credits: "",
+    creditos: [
+      "Un proyecto desarrollado por <b>LADOBLEP</b>:",
+      "<b>Paula Alberola y Paula Abella Tello</b>",
+    ]
   },
   {
     id: "setmestre",
     title: "SET MESTRE",
-    video_concept: [],
+    titulo_detalle: "SET MESTRE X Teka",
+    video_concept: [
+      ""
+    ],
     images: [
-      '/fotos/proyectos/moodboard teka/5.png',
       '/fotos/proyectos/moodboard teka/6.png',
       '/fotos/proyectos/moodboard teka/7.png',
       '/fotos/proyectos/moodboard teka/9.png',
       '/fotos/proyectos/moodboard teka/hecho.png',
     ],
-    subTitulos: [],
-    description: [],
+    poster: "/fotos/proyectos/moodboard teka/set_mestre_portada.png",
+    subTitulos: [
+      "Primer Premio Push the Power",
+      "Brother Valencia x Set Mestre",
+      "LADOBLEP 2025"
+    ],
+    description: [
+      `Durante el <b>Brother Valencia Summer School 2025</b>, desarrollamos <b>Set Mestre</b>, la campaña ganadora del concurso <b>Push the Power</b> para <b>TEKA</b>.
+       Una acción creada para las <b>placas de inducción Mestre Paeller</b>, que transforma el momento de cocinar una paella en un <b>juego interactivo</b>. 
+       A través de <b>pulseras inteligentes conectadas a la placa</b>, los comensales pueden opinar, interrumpir o competir mientras la app detecta y <b>penaliza a los más reincidentes</b>, coronando al auténtico mestre paeller.`
+    ],
     link: "/setmestre",
     video_concept_credits: "",
+    creditos: [
+      "Un proyecto desarrollado por <b>LADOBLEP</b>:",
+      "<b>Paula Alberola y Paula Abella Tello</b>",
+    ]
   }
   // {
   //   id: "",
